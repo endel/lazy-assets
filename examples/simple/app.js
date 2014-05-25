31c62fdd6358789333b2d7e7430f1fdfb4b481a4
Endel Dreyer <endel.dreyer@gmail.com>
2014-05-25 19:50:17 -0300

    add examples directory

---
+++ examples/simple/app.js
@@ -0,0 +1,4 @@
+var fs = require('fs'),
+    config = JSON.parse(fs.readFileSync(__dirname + '/../../.bowerrc'));
+
+console.log("Hello from browserify: ", config);
---
+++ examples/simple/test.coffee
@@ -0,0 +1,6 @@
+class Test
+  constructor: ->
+    console.log("Hello from coffee-script")
+
+
+t = new Test
