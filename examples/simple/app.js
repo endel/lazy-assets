db64e09e3d11da7bcc4c9db4fddbde585c22e48e
Endel Dreyer <endel.dreyer@gmail.com>
2014-08-03 04:30:49 -0300

    improve feedback when installing bower components. fix html livereload

--- examples/simple/app.js
+++ examples/simple/app.js
@@ -2,3 +2,13 @@ var fs = require('fs'),
     config = JSON.parse(fs.readFileSync(__dirname + '/../../.bowerrc'));
 
 console.log("Hello from browserify: ", config);
+
+$(function() {
+  $('body').on('click', function() {
+    console.log("You just clicked on body element.");
+  });
+
+  $('body').on('click', '.btn.btn-lg.btn-success', function() {
+    console.log("You just clicked on signup element.");
+  })
+});
